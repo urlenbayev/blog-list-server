f30d95b4065e7f976355ef65d9e68ed9604f1528
Akbar <url.akbar13@gmail.com>
2024-07-12 19:37:23 +0300

    Database setup *Define blog schema *Define model *Database connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3001
+
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.DB_CONNECTION_URL
+mongoose.connect(mongoUrl)
 
 
 app.listen(port, () => {
