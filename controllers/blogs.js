3d331d18d58ea5603b67f651e137accf7475d976
Akbar <url.akbar13@gmail.com>
2024-07-16 18:41:49 +0300

    Switch Blogs router from .then() to async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,18 +4,34 @@ const Blog = require('../models/Blog')
 
 //GET http://localhost:3001/api/blogs
 //Result is an array of blogs with title, author, url and likes
-blogsRouter.get('/', (req, res) => {
-  Blog.find({})
-    .then(blogs => {res.json(blogs)})
+/* 
+[
+  {
+    "title": "Introduction to MongoDB",
+    "author": "John Doe",
+    "url": "https://example.com/mongodb-intro",
+    "likes": 15,
+    "id": "66917cb6dda6d7bed93e0618"
+  },
+  ...
+] 
+*/
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
 })
 
 //POST http://localhost:3001/api/blogs
 //For creating a blog
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', async (req, res) => {
   const blog = new Blog(req.body)
-  blog.save()
-    .then(result => {res.status(201).json(result)})
-    .catch(error => next(error))
+  
+  try {
+    const result = await blog.save()
+    res.status(201).json(result)
+  } catch(error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
