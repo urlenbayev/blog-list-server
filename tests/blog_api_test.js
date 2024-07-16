43bc7cd731dc68240bed75ded65bed070c11b553
Akbar <url.akbar13@gmail.com>
2024-07-16 20:08:06 +0300

    Test /api/blogs endpoint for json format & correct amount of blogs

--- tests/blog_api_test.js
+++ tests/blog_api_test.js
@@ -1,12 +1,37 @@
-const {test, after} = require('node:test')
+const {test, after, beforeEach} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/Blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
+const testBlogs = 
+ [
+  {
+    "title": "Creating Your First RESTful API Service with Rails",
+    "author": "Ajay Naik",
+    "url": "https://medium.com/@ajay.naik44/creating-your-first-restful-api-service-with-rails-b7a39d59656a",
+    "likes": 0,
+  },
+  {
+    "title": "How to have background jobs in Rails",
+    "author": "Mateo mojica",
+    "url": "https://mateo-mojica.medium.com/how-to-have-background-jobs-in-rails-e4c93ded96cf",
+    "likes": 50,
+  },
+ ]
+
+ beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(testBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(testBlogs[1])
+  await blogObject.save()
+})
+
+test.only('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -15,10 +40,10 @@ test('blogs are returned as json', async () => {
 })
 
 
-test('there are 12 blogs', async () => {
+test.only('there are 2 blogs', async () => {
   const res = await api.get('/api/blogs')
 
-  assert.strictEqual(res.body.length, 12)
+  assert.strictEqual(res.body.length, 2)
 })
 
 test('the first blog is about MongoDB', async () => {
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const port=process.env.PORT
-const mongoUrl = process.env.NODE_ENV='development'
+const mongoUrl = (process.env.NODE_ENV==='development' 
+  || process.env.NODE_ENV==='production')
   ? process.env.DB_CONNECTION_URL
   : process.env.TEST_DB_CONNECTION_URL
 
